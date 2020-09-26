be7e3f4bd55d0460c8cff5e349b3c3db8114b74e
oliveiraFAB16 <fabriciocarvalho932@gmail.com>
2020-09-25 23:19:52 -0300

    somaDeNumeros

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Operções com inteiros");
+            int X = 70;
+            int Y = 30;
+            int soma = X + Y;
+            Console.WriteLine($"{X} + {Y} = {soma}");
+            
+
+            
+
+
+            
+        }
+    }
+}
